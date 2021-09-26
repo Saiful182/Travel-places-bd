d6691f6d2de4e877b2311e83b35ea80fe3206d19
Saiful182 <saifulislamcse0@gamil.com>
2021-09-26 21:20:56 +0600

    done

--- src/component/place/place.js
+++ src/component/place/place.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './place.css';
+import { FcRating } from "react-icons/fc";
+import { FcApproval } from "react-icons/fc";
 
 
 const Place = (props) => {
@@ -12,9 +14,9 @@ const Place = (props) => {
                 <h4>Place Name: {name}</h4>
                 <h4>City Name: {city}</h4>
                 <h4>Stay: {days} days</h4>
-                <h4>rating: {rating} </h4>
+                <h4 className="rating">Rating: {rating} <FcRating className="icon" /> </h4>
                 <h3>Pakage Price: {budget} Taka</h3>
-                <button onClick={() => props.clickHandel(props.place)} className="place-button">Select this pakage</button>
+                <button onClick={() => props.clickHandel(props.place)} className="place-button"> <FcApproval className="icon-button" /> Select this pakage</button>
 
             </div>
         </div>
